cbb956e0969ee3ff852c24d104bb528a9a270494
Lucas <lucas.souza@qwasolucoes.com.br>
2026-05-04 14:08:32 -0300

    add exercicios 01,02 e 03

---
+++ src/EstruturaSequencial/Exercicio02.java
@@ -0,0 +1,22 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        /*  Faça um programa para ler o valor do raio de um círculo,
+            e depois mostrar o valor da área deste círculo com
+            quatro casas decimais conforme exemplos.
+
+            Area = pi.raio²
+            pi = 3.14159
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o Raio do Circulo");
+        double raio = sc.nextDouble();
+
+        double area = 3.14159 * Math.pow(raio,2);
+
+        System.out.printf("Area = %.4f %n", area);
+    }
+}
---
+++ src/EstruturaSequencial/Exercicio03.java
@@ -0,0 +1,20 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d;
+        System.out.println("diferença do produto\n" +
+                "de A e B pelo produto de C e D: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        int diferenca = a*b - c*d;
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+    }
+}
