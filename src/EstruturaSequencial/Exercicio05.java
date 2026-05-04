cc06bd14798a044e9cbd81a05cc132d35d536f2d
Lucas <lucas.souza@qwasolucoes.com.br>
2026-05-04 14:50:48 -0300

    add Exercicio04 and Exercicio05

---
+++ src/EstruturaSequencial/Exercicio05.java
@@ -0,0 +1,20 @@
+package EstruturaSequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a peça, quantidade e valor unitário: (12 1 5.30)");
+        int peca = sc.nextInt();
+        int qtdPeca = sc.nextInt();
+        double valPeca = sc.nextDouble();
+
+        double totalAPagar = (double)qtdPeca * valPeca;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f %n", totalAPagar);
+    }
+}
