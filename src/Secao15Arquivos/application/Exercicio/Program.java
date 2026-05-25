af65a7fac3a2ecb61d5b70abf72a3393e278bcb8
Lucas <lucas.souza@qwasolucoes.com.br>
2026-05-25 12:13:03 -0300

    add secao15

---
+++ src/Secao15Arquivos/application/Exercicio/Program.java
@@ -0,0 +1,58 @@
+package Secao15Arquivos.application.Exercicio;
+
+import Secao15Arquivos.application.Exercicio.entities.Item;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Item> list = new ArrayList<>();
+
+        System.out.println("Enter file path: ");
+        String filePath = sc.nextLine();
+
+        File sourceFile = new File(filePath);
+        String sourceFolder = sourceFile.getParent(); // Retorna o nome de caminho abstrato do "pai".
+
+        boolean sucess = new File(sourceFolder + "\\out").mkdir();
+
+        String targetFile = sourceFolder + "\\out\\sumary.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(sourceFile))) {
+            String itemCsv = br.readLine();
+            while (itemCsv != null){
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Item(name, quantity, price));
+
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFile))){
+                for (Item i : list){
+                    bw.write(i.getName()+", "+String.format("%.2f", i.calculateTotalValue()));
+                    bw.newLine();
+                }
+                System.out.println(targetFile + " CREATED!");
+
+            }
+            catch (IOException e){
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: "+ e.getMessage());
+        }
+        sc.close();
+    }
+}
---
+++ src/Secao15Arquivos/application/Exercicio/entities/Item.java
@@ -0,0 +1,43 @@
+package Secao15Arquivos.application.Exercicio.entities;
+
+public class Item {
+    private String name;
+    private Integer quantity;
+    private Double price;
+
+    public Item(){}
+
+    public Item(String name, Integer quantity, Double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double calculateTotalValue(){
+        return quantity * price;
+    }
+}
